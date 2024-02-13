d5a6b755a14417182af26f46df7fb247bb594d78
Chris <32988106+Noobot9k@users.noreply.github.com>
2024-02-13 14:34:05 -0700

    Room template and placeholder art

---
+++ Assets/Objects/RoomGeneric/Room.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+
+    public GameObject DoorExit;
+    public GameObject DoorEnterance;
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+}
