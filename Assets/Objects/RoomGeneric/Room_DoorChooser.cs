9f507637001698bbc9d822d53022d631627eea1b
Chris <32988106+Noobot9k@users.noreply.github.com>
2024-02-13 15:27:39 -0700

    Emoji experiment

---
+++ Assets/Objects/RoomGeneric/Room_DoorChooser.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room_DoorChooser : Room {
+
+    public List<GameObject> Doors;
+
+    void Start() {
+
+    }
+    void Update() {
+
+    }
+}
