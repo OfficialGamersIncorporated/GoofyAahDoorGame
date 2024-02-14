f2c966a7e7821da833997423e2ed6110891386ff
bryce <SwPGameDev@gmail.com>
2024-02-13 20:33:07 -0700

    Ideas list

--- Assets/Scenes/SwPTest/TestPlayerMovement.cs
+++ Assets/Scenes/SwPTest/TestPlayerMovement.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class TestPlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    GameObject player;
+    [SerializeField] float moveSpeed;
+    [SerializeField] Vector3 movementVector;
+    public float PlayerInputHoriziontal;
+    public float PlayerInputVertical;
 
-    // Update is called once per frame
-    void Update()
+
+    private void Start()
     {
-        
+        player = gameObject;
+    }
+    private void Update()
+    {
+        PlayerInputHoriziontal = Input.GetAxis("Horizontal");
+        PlayerInputVertical = Input.GetAxis("Vertical");
+
+        movementVector = moveSpeed * Time.deltaTime * new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+        if (movementVector != Vector3.zero)
+        {
+            player.transform.position += movementVector;
+        }
     }
 }
